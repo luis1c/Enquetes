7a512c40534ddbb0b450789bb40c4598f4ecefae
Luís Carlos <lcscamara.cic@uesc.br>
2024-02-12 10:39:58 -0300

    Fazendo conecção com o DB através da ORM prisma.io, criando tabela Poll, criando rota HTTP para nova enquete (rota testada através do cliente HTTP 'hoppscotch')

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,28 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'Bem vindo ao seu projeto'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request, replay) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+
+    return replay.status(201).send({ pollId: poll.id }) //código 201 para requisição bem sucedida e novo recurso criado
 }) 
 
 app.listen({port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
+
